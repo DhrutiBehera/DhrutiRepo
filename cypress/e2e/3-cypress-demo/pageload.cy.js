fef3dbeb163754751dc2dd0ba2a45c0e4449259e
Dhruti <dhrutisunder_behera@gmail.com>
2022-07-28 20:50:07 +0530

    new code repo added

--- cypress/e2e/3-cypress-demo/pageload.cy.js
+++ cypress/e2e/3-cypress-demo/pageload.cy.js
@@ -1,11 +1,11 @@
 context('Page Load Test', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:8080/commands/actions')
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/commands/actions')
+  })
 
-    it('Check if Action page loaded fine', () => {
-        cy.get('h1').should('exist')
-        //cy.get('h1').should('contain.text', 'Actions')
-        cy.get('h1').should('have.text', 'Actions')
-    })
+  it('Check if Action page loaded fine', () => {
+    cy.get('h1').should('exist')
+    //cy.get('h1').should('contain.text', 'Actions')
+    cy.get('h1').should('have.text', 'Actions')
+  })
 })
